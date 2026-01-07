91a6928eff77335bf9fb5f49092b110299de67fc
Amit Karane <116144740+Amitkarane@users.noreply.github.com>
2026-01-07 20:44:17 +0530

    first commit

---
+++ src/main/java/com/Assignment/SpringAIProject/Assignment/Controller/chatController.java
@@ -0,0 +1,28 @@
+package com.Assignment.SpringAIProject.Assignment.Controller;
+
+import org.apache.coyote.Response;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+public class chatController {
+
+
+    private ChatClient chatClient;
+
+    public chatController(ChatClient.Builder builder){
+        this.chatClient=builder.build();
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(@RequestParam(value="q") String query){
+        String responseContent = this.chatClient.prompt(query).call().content();
+        return ResponseEntity.ok(responseContent);
+    }
+}
